2dced2fb75c2f5fc3cc79f899ebf058cfde36356
mhkmcp <mhkmcp@yahoo.com>
2021-10-01 22:40:00 +0600

    responsive added

--- gsc_project/settings.py
+++ gsc_project/settings.py
@@ -136,3 +136,8 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static-root')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+)
