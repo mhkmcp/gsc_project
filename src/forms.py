f259004cf6abaf6b5a8a6653abbbfdff7aa9bed1
rakinul-islam-raju <rakibul.islam7772588@gmail.com>
2021-10-03 20:44:04 +0600

    Fix member model

--- src/forms.py
+++ src/forms.py
@@ -1,4 +1,4 @@
-from django import forms 
+from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm, UsernameField
 
@@ -6,37 +6,40 @@ from .models import Member
 
 
 class CustomUserCreationForm(UserCreationForm):
-
     class Meta:
         model = User
-        fields = ['email', 'password1', 'password2']
+        fields = ["email", "password1", "password2"]
 
 
 class MemberForm(forms.ModelForm):
     class Meta:
         model = Member
-        fields = ['full_name', 'passport',  'date_of_birth', 'city', 'country', 'member_type']
-        # exclude = ()
+        exclude = [
+            "user",
+            "is_approved",
+        ]
 
 
 month_choice = (
-    (1, 'জানুয়ারী'),
-    (2, 'ফেব্রুয়ারী'),
-    (3, 'মার্চ'),
-    (4, 'এপ্রিল'),
-    (5, 'মে'),
-    (6, 'জুন'),
-    (7, 'জুলাই'),
-    (8, 'অগাস্ট'),
-    (9, 'সেপ্টেম্বর'),
-    (10, 'অক্টোবর'),
-    (11, 'নভেম্বর'),
-    (12, 'ডিসেম্বর')
+    (1, "জানুয়ারী"),
+    (2, "ফেব্রুয়ারী"),
+    (3, "মার্চ"),
+    (4, "এপ্রিল"),
+    (5, "মে"),
+    (6, "জুন"),
+    (7, "জুলাই"),
+    (8, "অগাস্ট"),
+    (9, "সেপ্টেম্বর"),
+    (10, "অক্টোবর"),
+    (11, "নভেম্বর"),
+    (12, "ডিসেম্বর"),
 )
 
 
 class QueryForm(forms.Form):
-    month = forms.ChoiceField(label='Select a Month', choices=month_choice, widget=forms.Select)
+    month = forms.ChoiceField(
+        label="Select a Month", choices=month_choice, widget=forms.Select
+    )
     year = forms.IntegerField(min_value=2000, max_value=3000)
 
 
@@ -45,4 +48,4 @@ class LoginForm(forms.ModelForm):
 
     class Meta:
         model = User
-        fields = ['username', 'password']
+        fields = ["username", "password"]
