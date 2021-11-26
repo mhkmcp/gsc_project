b20076f5c98f00d5180b4e72ac3741362a1b8e85
mhkmcp <mhkmcp@yahoo.com>
2021-11-27 00:23:00 +0600

    menu resonsive updated

--- gsc_project/settings.py
+++ gsc_project/settings.py
@@ -70,19 +70,21 @@ WSGI_APPLICATION = 'gsc_project.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    # }
-
+    # local
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'bcpiskp_gsc',
-        'USER': 'bcpiskp_admin123',
-        'PASSWORD': '9DgsKbA@f@@Y',
-        'HOST': '127.0.0.1',   # Or an IP Address that your DB is hosted on
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+
+    # development
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'NAME': 'bcpiskp_gsc',
+    #     'USER': 'bcpiskp_admin123',
+    #     'PASSWORD': '9DgsKbA@f@@Y',
+    #     'HOST': '127.0.0.1',   # Or an IP Address that your DB is hosted on
+    #     'PORT': '',
+    # }
 }
 
 
