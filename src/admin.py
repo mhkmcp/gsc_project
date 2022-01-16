1c80005c0f32b1f67a7c05453b17310cbb4e93a8
rakinul-islam-raju <rakibul.islam7772588@gmail.com>
2022-01-16 21:51:06 +0600

    add:added commission model

--- src/admin.py
+++ src/admin.py
@@ -2,6 +2,7 @@ from django.contrib.admin.sites import AlreadyRegistered
 from django.contrib.auth.models import Group, User
 from django.contrib.auth.admin import UserAdmin
 from django.contrib import admin
+from django.shortcuts import get_object_or_404
 from django.apps import apps
 
 from .forms import GroupAdminForm
@@ -14,50 +15,19 @@ from .models import (
     Slide,
     Election,
     Candidate,
+    Commission,
 )
-
-# unregister default Group model
-admin.site.unregister(Group)
-
-# unregister default User model
-admin.site.unregister(User)
-
-
-@admin.register(Group)
-class GroupAdmin(admin.ModelAdmin):
-    form = GroupAdminForm
-    filter_horizontal = ["permissions"]
-
-
-@admin.register(User)
-class GroupAdmin(admin.ModelAdmin):
-    list_display = [
-        "username",
-        "email",
-        "member_passport",
-        "is_active",
-        "is_staff",
-    ]
-    list_display_links = [
-        "username",
-        "email",
-        "member_passport",
-    ]
-    search_fields = ["username", "email"]
-    list_filter = [
-        "is_active",
-        "is_staff",
-    ]
-    date_hierarchy = "date_joined"
-
-    def member_passport(self, x):
-        return x.member.passport
-
-    member_passport.short_description = "Passport"
 
 
 @admin.register(Member)
 class MemberAdmin(admin.ModelAdmin):
+    def add_to_election_commission(self, request, queryset):
+        election_commission = get_object_or_404(Commission, name="Election Commission")
+        queryset.update(election_commission_member=election_commission)
+
+    def remove_from_election_commission(self, request, queryset):
+        queryset.update(election_commission_member=None)
+
     def make_member_approved(self, request, queryset):
         queryset.update(is_approved=True)
 
@@ -66,6 +36,10 @@ class MemberAdmin(admin.ModelAdmin):
 
     make_member_approved.short_description = "Update Member to Approve"
     make_member_unapproved.short_description = "Update Member to Unapproved"
+    add_to_election_commission.short_description = "Add Member to Election Commission"
+    remove_from_election_commission.short_description = (
+        "Remove Member from Election Commission"
+    )
 
     list_display = [
         "full_name",
@@ -73,13 +47,26 @@ class MemberAdmin(admin.ModelAdmin):
         "member_type",
         "phone",
         "zilla",
+        "election_commission",
         "is_approved",
     ]
     list_filter = [
         "is_approved",
         "member_type",
     ]
-    actions = [make_member_approved, make_member_unapproved]
+    search_fields = ["passport", "phone", "user__email"]
+    actions = [
+        make_member_approved,
+        make_member_unapproved,
+        add_to_election_commission,
+        remove_from_election_commission,
+    ]
+
+    def election_commission(self, x):
+        if x.election_commission_member:
+            return True
+        else:
+            return "-"
 
 
 @admin.register(Subscription)
