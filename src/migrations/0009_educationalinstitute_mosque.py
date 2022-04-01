f60a482d4cdaff90d697c6fff99f9574d9e66fb3
rakinul-islam-raju <rakibul.islam7772588@gmail.com>
2022-04-01 12:46:07 +0600

    [FIX] migration files

--- src/migrations/0009_educationalinstitute_mosque.py
+++ src/migrations/0009_educationalinstitute_mosque.py
@@ -6,27 +6,52 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('src', '0008_auto_20220329_2208'),
+        ("src", "0008_auto_20220329_2208"),
     ]
 
     operations = [
         migrations.CreateModel(
-            name='EducationalInstitute',
+            name="EducationalInstitute",
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('name', models.CharField(max_length=254)),
-                ('headmaster', models.CharField(blank=True, default='', max_length=100, null=True)),
-                ('school_code', models.CharField(blank=True, default='', max_length=100, null=True)),
-                ('stablished_at', models.DateField(blank=True, default='', null=True)),
+                (
+                    "id",
+                    models.AutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("name", models.CharField(max_length=254)),
+                (
+                    "headmaster",
+                    models.CharField(blank=True, default="", max_length=100, null=True),
+                ),
+                (
+                    "school_code",
+                    models.CharField(blank=True, default="", max_length=100, null=True),
+                ),
+                ("stablished_at", models.DateField(blank=True, null=True)),
             ],
         ),
         migrations.CreateModel(
-            name='Mosque',
+            name="Mosque",
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('name', models.CharField(max_length=254)),
-                ('imam', models.CharField(blank=True, default='', max_length=100, null=True)),
-                ('stablished_at', models.DateField(blank=True, default='', null=True)),
+                (
+                    "id",
+                    models.AutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("name", models.CharField(max_length=254)),
+                (
+                    "imam",
+                    models.CharField(blank=True, default="", max_length=100, null=True),
+                ),
+                ("stablished_at", models.DateField(blank=True, null=True)),
             ],
         ),
     ]
